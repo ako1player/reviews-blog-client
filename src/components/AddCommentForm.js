849821419e558ea26af50b46b4a3532552474158
Adrian Garcia Rios <adrigar10@gmail.com>
2023-01-01 14:14:57 -0500

    changes

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -23,7 +23,7 @@ const AddCommentForm = ({movieName, onMovieUpdated}) =>{
     return(
         <div className="pb-2">
             <h3 className="text-xl">Add Comment</h3>
-            {user && <p>You are posting as {user.email}</p>}
+            {user && <p>You are posting as {user.name}</p>}
             <div>
                 <textarea className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 placeholder="Write you comment here..."
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,7 @@ const HomePage = () =>{
     return (
         <div className="text-center text-jadeGreen-800">
             <h1 className="text-3xl pb-10">Welcome to my quick reviews of movies and anime</h1>
+            <p>The purpose of this is to keep track of movies/anime I have watched.</p>
             <div className="p-2">
                 <h1>Recent Movie Reviews:</h1>
                 <RecentMovies movies={movies} />
